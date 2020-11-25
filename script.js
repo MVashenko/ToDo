2d51aba878cc9e23c318c2045e2eb15e5bd911c7
v.vashchenko <v.vashchenko@wsmgroup.ru>
2020-11-25 17:21:25 +0200

    провел ревью. оставил комментарии.

--- script.js
+++ script.js
@@ -1,34 +1,47 @@
-console.log('log')
+
 $(function() {
-	var $tasksList = $("#tasksList")
-	var $tasksInput = $("#tasksInput")
-	var $notification = $("#notification")
+  // плохо использовать var ( лучше использовать let или const ) почитай, что это такое
+  var $tasksList = $("#tasksList")
+  var $tasksInput = $("#tasksInput")
+  var $notification = $("#notification")
 
+// нейминг не отражает сути -
+  var displayNotification = function(){
+    if (!$tasksList.children().length){
+      $notification.fadeIn("fast")
+    } else{
+      $notification.css("display", "none")
+    }
+  }
 
-	var displayNotification = function(){
-		if (!$tasksList.children().length){
-			$notification.fadeIn("fast")
-		} else{
-			$notification.css("display", "none")
-		}
-	}
+  $("#taskAdd").on("click", function() {
+    // не нужно возвращать что либо если тебе нужно просто ничего не делать можно писать просто return
+    if(!$tasksInput.val()) {return false;}
+    // вот это предлагаю вынести в функцию getItem, которая будет принимать $tasksInput.val() и выдавать элемент
 
-	$("#taskAdd").on("click", function() {
-		if(!$tasksInput.val()) {return false;}
+    // также, конкатенация строк через + прошлый век прочитай про темплейтные строчки `` в них ты можешь делать так
+    // const a = 4
+    // const string = `${a}$`
+    // 4$
+    $tasksList.append("<li>" + $tasksInput.val() + "<button class='delete'>&#10006</button></li>")
 
-		$tasksList.append("<li>" + $tasksInput.val() + "<button class='delete'>&#10006</button></li>")
+    $tasksInput.val("")
 
-		$tasksInput.val("")
-
-		displayNotification()
-
-		$(".delete").on("click", function(){
-			var $parent = $(this).parent()
-			$parent.css("animation", "fadeOut .3s linear")	
-			setTimeout(function(){
-				$parent.remove()
-				displayNotification()
-			}, 250)
-		})
-	})
+    displayNotification()
+    // вообще задача была не удалять а помечать выполненным
+    // передлагаю каллбек вынести также как функцию displayNotification
+    $(".delete").on("click", function(){
+      var $parent = $(this).parent()
+      // пропиши стили сразу в css и по событию меняй класс
+      $parent.css("animation", "fadeOut .3s linear")
+      setTimeout(function(){
+        $parent.remove()
+        displayNotification()
+      }, 250)
+    })
+  })
 })
+
+// задание тебе дальше:
+// 1. внести правки выше
+// 2. добавить фильтр ( кнопка которая будет отделять выполненные от невыполенных и можно будет посмотреть отдельно списки)
